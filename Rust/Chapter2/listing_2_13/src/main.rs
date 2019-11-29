a613dcc1d304b1472643eeedcf1276ecf8aaff38
Bert de Miranda <info@bertdemiranda.nl>
2019-11-29 12:50:31 +0100

    Add listing_2_13

---
+++ Rust/Chapter2/listing_2_13/src/main.rs
@@ -0,0 +1,47 @@
+#![allow(dead_code)]
+
+use bigdecimal::BigDecimal;
+use std::str::FromStr;
+use std::collections::HashMap;
+
+fn bigdec(s: &str) -> BigDecimal {
+    BigDecimal::from_str(s).unwrap()
+}
+
+struct SavingsAccount {
+    rate: BigDecimal,
+}
+
+fn calculate_interest(account: &SavingsAccount, balance: &BigDecimal) -> Result<BigDecimal, String> {
+    if account.rate == bigdec("0") {
+        Err(String::from("Interest Rate nog found"))
+    } else {
+        Ok(bigdec("1000"))
+    }
+}
+
+fn get_currency_balance(account: &SavingsAccount) -> Result<BigDecimal, String> {
+    Ok(bigdec("1000"))
+} 
+
+fn calculate_net_asset(account: &SavingsAccount, ccy_balance: &BigDecimal, interest: BigDecimal) -> Result<BigDecimal, String> {
+    Ok(ccy_balance + interest + bigdec("200"))
+}
+
+fn do_calculate(account: &SavingsAccount) -> Result<(&SavingsAccount, BigDecimal), String> {
+    let b = get_currency_balance(account)?;
+    let i = calculate_interest(account, &b)?;
+    let v = calculate_net_asset(account, &b, i)?;
+    Ok((account, v))
+}
+
+fn main() {
+    let s1 = SavingsAccount {
+        rate: bigdec("0.2"),
+    };
+
+    match do_calculate(&s1) {
+        Ok((acc, nv)) => println!("Hello, net assets of {}!", nv),
+        Err(e)        => println!("Error {}!", e)
+    }
+}
