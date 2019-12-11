52702f5ec5d57319c9d1e5d1cf2e914f257c5cc5
Bert de Miranda <info@bertdemiranda.nl>
2019-12-11 16:01:59 +0100

    Extended listing 3_4

--- Rust/Chapter3/listing_3_4/src/main.rs
+++ Rust/Chapter3/listing_3_4/src/main.rs
@@ -1,17 +1,56 @@
 #![allow(dead_code)]
 
-#[derive(Debug)]
-enum DayOfWeek {
-    Monday,
-    Tuesday,
-    Wednesday,
-    Thursday,
-    Friday,
-    Saturday,
-    Sunday
+mod dow {
+    #[derive(Debug)]
+    pub enum DayOfWeek {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+
+    impl DayOfWeek {
+        pub fn day_of_week(d: u8) -> Option<DayOfWeek> {
+            match d {
+                1 => Some(DayOfWeek::Monday),
+                2 => Some(DayOfWeek::Tuesday),
+                3 => Some(DayOfWeek::Wednesday),
+                4 => Some(DayOfWeek::Thursday),
+                5 => Some(DayOfWeek::Friday),
+                6 => Some(DayOfWeek::Saturday),
+                7 => Some(DayOfWeek::Sunday),
+                _ => None
+            }
+        }
+
+        pub fn to_string(&self) -> String {
+            match self {
+                DayOfWeek::Monday    => String::from("Monday"),
+                DayOfWeek::Tuesday   => String::from("Tuesday"),
+                DayOfWeek::Wednesday => String::from("Wednesday"),
+                DayOfWeek::Thursday  => String::from("Thursday"),
+                DayOfWeek::Friday    => String::from("Friday"),
+                DayOfWeek::Saturday  => String::from("Saturday"),
+                DayOfWeek::Sunday    => String::from("Sunday"),
+            }
+        }
+    }
 }
 
 fn main() {
+    use dow::*;
+
     let dow = DayOfWeek::Thursday;
-    println!("Hello, day of week {:?}!", dow);
+    println!("Hello, day of week {}!", dow.to_string());
+
+    let dow2 = DayOfWeek::day_of_week(2).unwrap();
+    println!("Hello, day of week 2 {}!", dow2.to_string());
+
+    match DayOfWeek::day_of_week(8) {
+        Some(dow) => println!("Hello, day of week 3 {}!", dow.to_string()),
+        None      => println!("Hello, no day of week!")
+    }
 }
