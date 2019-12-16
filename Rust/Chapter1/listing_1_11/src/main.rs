fe43ef64b836968992e34dc7e2be6bb5444ae829
Bert de Miranda <info@bertdemiranda.nl>
2019-12-16 16:22:00 +0100

    Use more &chr as parameter

--- Rust/Chapter1/listing_1_11/src/main.rs
+++ Rust/Chapter1/listing_1_11/src/main.rs
@@ -46,8 +46,8 @@ pub struct Account {
 }
 
 impl Account {
-    fn new(no: String, name: String, date_of_opening: Date<Local>) -> Account {
-        Account {no, name,
+    fn new(no: &str, name: &str, date_of_opening: Date<Local>) -> Account {
+        Account {no: String::from(no), name: String::from(name),
             date_of_opening,
             balance: Amount::new(0),
         }
@@ -58,12 +58,18 @@ pub struct Customer {
     name: String,
 }
 
+impl Customer {
+    fn new(name: &str) -> Customer {
+        Customer {name: String::from(name)}
+    }
+}
+
 // Verification of customers module
 // --------------------------------
 mod verifications {
     use crate::Customer;
 
-    pub fn verify_record(customer: Customer) -> Result<Customer, String> {
+    pub fn verify_record(_customer: Customer) -> Result<Customer, String> {
         //Ok(customer)
         Err(String::from("We don't know this person!!!"))
     }
@@ -74,7 +80,7 @@ mod verifications {
 mod account_service {
     use chrono::prelude::*;
     use crate::Account;
-    use crate::Amount;
+    //...use crate::Amount;
     use crate::Customer;
     use crate::verifications::verify_record;
 
@@ -84,15 +90,14 @@ mod account_service {
 
     pub fn open_checking_account(customer: Customer, effective_date: Date<Local>) -> Result<Account, String> {
         //..
-        let no = String::from("acc1");
-        Ok(Account::new(no, customer.name, effective_date))
+        Ok(Account::new("acc1", &customer.name, effective_date))
     }
 }
 
 fn main() {
     use account_service::{verify_customer, open_checking_account};
 
-    let cust = Customer { name: String::from("customer1") };
+    let cust = Customer::new("customer1");
     match
         verify_customer(cust)
             .and_then(|c| open_checking_account(c, Local::now().date())) {
