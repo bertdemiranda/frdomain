d2c0a12c60fe58a83df9316a866c4132cd8b2685
Bert de Miranda <info@bertdemiranda.nl>
2019-12-18 12:46:57 +0100

    impl InterestBearingAccount as a trait

--- Rust/Chapter2/listing_2_2/src/main.rs
+++ Rust/Chapter2/listing_2_2/src/main.rs
@@ -15,63 +15,76 @@ struct Account {
     name:   String,
 }
 
+trait InterestBearingAccount {
+    fn interest_rate(&self) -> Amount;
+}
+
 struct CheckingAccount {
     base_data: Account,
 }
 
-struct InterestBearingAccount
-{
-    rate_of_interest: BigDecimal,
-}
-
-impl InterestBearingAccount {
-    fn calc_interest(&self, _period: Duration) -> Result<Amount, String> {
-        Ok(bigdec("1.5")) 
-    }
-}
-
 struct SavingsAccount {
-    base_data:     Account,
-    interest_data: InterestBearingAccount,
+    base_data:        Account,
+    rate_of_interest: BigDecimal,
 }
 
 impl SavingsAccount {
     fn new(base_data: Account, rate_of_interest: BigDecimal) -> SavingsAccount {
         SavingsAccount {
             base_data, 
-            interest_data: InterestBearingAccount { rate_of_interest },
+            rate_of_interest,
         }
     }
+}
 
-    fn calculate_interest(&self, _period: Duration) -> Result<Amount, String> {
-        self.interest_data.calc_interest(_period)
+impl InterestBearingAccount for SavingsAccount {
+    fn interest_rate(&self) -> Amount {
+        self.rate_of_interest.clone()
     }
 }
 
 struct MoneyMarketAccount {
-    base_data:     Account,
-    interest_data: InterestBearingAccount,
+    base_data:        Account,
+    rate_of_interest: BigDecimal,
 }
 
 impl MoneyMarketAccount {
-    // fn new(base_data: Account, rate_of_interest: BigDecimal) -> MoneyMarketAccount {
-    //     // ...
-    // }
-
-    fn calculate_interest(&self, _period: Duration) -> Result<Amount, String> {
-        self.interest_data.calc_interest(_period)
+    fn new(base_data: Account, rate_of_interest: BigDecimal) -> SavingsAccount {
+        SavingsAccount {
+            base_data, 
+            rate_of_interest,
+        }
     }
+}
+
+impl InterestBearingAccount for MoneyMarketAccount {
+    fn interest_rate(&self) -> Amount {
+        self.rate_of_interest.clone()
+    }
+}
+
+fn calculate_interest(account: impl InterestBearingAccount, _period: Duration) -> Result<Amount, String> {
+    Ok(account.interest_rate()) 
 }
 
 fn main() {
     let dur = 1;
 
-    let b = SavingsAccount::new(
+    let s = SavingsAccount::new(
         Account {
             number: String::from("acc2"),
             name:   String::from("john"),
         },
         BigDecimal::from_str(&String::from("2.5")).unwrap()
     );
-    println!("Interest = {:?}", b.calculate_interest(dur));
+    println!("Interest Savings Account = {:?}", calculate_interest(s, dur));
+
+    let m = MoneyMarketAccount::new(
+        Account {
+            number: String::from("acc3"),
+            name:   String::from("john"),
+        },
+        BigDecimal::from_str(&String::from("3.5")).unwrap()
+    );
+    println!("Interest Moneymarket Account = {:?}", calculate_interest(m, dur));
 }
